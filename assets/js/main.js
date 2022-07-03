34f3ae8f689799d0bb7002e49ed3e19c529058e0
Bernardo Marinho <bgmarinho96@gmail.com>
2022-07-03 16:24:58 -0300

    Saving-point

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,14 +6,31 @@ function scope() {
     const pausar = document.querySelector(".stop");
     const zerar = document.querySelector(".start-over");
 
+    function returnInitialTime() {
+      let data = new Date("01-01-1970 00:00:00");
+      return data.toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      });
+    }
+
     iniciar.addEventListener("click", function (event) {
-      alert("Cliquei no iniciar");
+      console.log(" ----- Cliquei no iniciar -----");
+      let initialTime = returnInitialTime();
+      console.log(initialTime);
+      console.log(
+        initialTime instanceof Date ? "é instância" : "não é instância"
+      );
     });
+
     pausar.addEventListener("click", function (event) {
-      alert("Cliquei no pausar");
+      console.log("Cliquei no pausar");
     });
+
     zerar.addEventListener("click", function (event) {
-      alert("Cliquei no zerar");
+      console.log("Cliquei no zerar");
     });
   }
 }
