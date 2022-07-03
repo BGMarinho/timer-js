62c822f338134c47eb3ab76fa1ca4a6948a25eca
Bernardo Marinho <bgmarinho96@gmail.com>
2022-07-03 15:28:30 -0300

    Adding the first js part

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,20 @@
 function scope() {
   const bodyClass = document.body.classList.contains("my-timer");
   if (bodyClass === true) {
+    const relogio = document.querySelector(".clock");
+    const iniciar = document.querySelector(".start");
+    const pausar = document.querySelector(".stop");
+    const zerar = document.querySelector(".start-over");
+
+    iniciar.addEventListener("click", function (event) {
+      alert("Cliquei no iniciar");
+    });
+    pausar.addEventListener("click", function (event) {
+      alert("Cliquei no pausar");
+    });
+    zerar.addEventListener("click", function (event) {
+      alert("Cliquei no zerar");
+    });
   }
 }
 scope();
